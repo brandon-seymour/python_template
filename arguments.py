ede538c7ac126c01027e173e675a9e719a67bdc6
brandon-seymour <59863256+brandon-seymour@users.noreply.github.com>
2023-03-20 19:40:40 -0700

    add argument

--- arguments.py
+++ arguments.py
@@ -3,5 +3,5 @@ import argparse
 def get():
     parser = argparse.ArgumentParser()
     parser.add_argument('-v', '--version', dest='version', default='1', help='Version')
-    parser.add_argument('-s', action='store_true', default=False, help="Default true")
+    parser.add_argument('-s', dest='save', action='store_true', default=False, help="Default true")
     return parser.parse_args()
